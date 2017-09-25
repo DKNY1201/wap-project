4c6e3cce74c0d152823d830b23104f2a308004c3
Quy Tran <quytran288@gmail.com>
2017-09-25 15:48:50 -0500

    Working on Validation for Sign up and Sign in pages

---
+++ src/main/java/edu/mum/utils/ValidationUtils.java
@@ -0,0 +1,24 @@
+package edu.mum.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Bi on 9/25/17.
+ */
+public class ValidationUtils {
+    private static final String PASSWORD_PATTERN = ".{8,}";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z_\\-]+@(([a-zA-Z_\\-])+\\.)+[a-zA-Z]{2,4}$";
+
+    public static boolean verifyPassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public static boolean verifyEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
